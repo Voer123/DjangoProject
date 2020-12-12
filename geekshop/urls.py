b7d6aeded7b9fc3aace4bf9e39d85f3e73ffa09d
Voer <valera-machcagarov@mail.ru>
2020-12-12 22:33:05 +0900

    Lesson2

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -5,6 +5,6 @@ from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.main),
-    path('products/', mainapp_views.products),
+    path('', mainapp_views.main, name='index'),
+    path('products/', mainapp_views.products, name='products'),
 ]
